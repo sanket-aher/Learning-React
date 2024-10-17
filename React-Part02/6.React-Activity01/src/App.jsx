393606d79aada2319439a5f97b51fc441feceef9
Sanket Aher <sanketaher7952@gmail.com>
2024-10-18 01:32:13 +0530

    React props Activity01

---
+++ React-Part02/6.React-Activity01/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MsgBox from './MsgBox';
+
+function App() {
+  return (
+    <>
+      <MsgBox userName="Smith" textColor="yellow" />
+      <MsgBox userName="Hayden" textColor="green" />
+    </>
+  )
+}
+
+export default App
