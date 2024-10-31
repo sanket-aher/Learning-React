7f650895d3a63cad944f125a0d8ff001dbbed403
Sanket Aher <sanketaher7952@gmail.com>
2024-10-31 23:56:21 +0530

    React Like Button Activity

---
+++ React-Part03/4.Activity-LikeButton/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import LikeButton from './LikeButton';
+
+// Q. Activity - Create Like Button.
+
+function App() {
+    return (
+    <>
+      <p>States in React</p>
+      <LikeButton />
+    </>
+  )
+}
+
+export default App
